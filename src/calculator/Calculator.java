4dc0e981aa1d8fd3745d09846e45cc5d11737da6
Dimitry Fedotov <dima@fedoto.ws>
2018-08-23 20:28:53 +0300

    calculate 2 operations

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,37 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        int a = Integer.parseInt(expression[0]);
+        int b = Integer.parseInt(expression[2]);
+        int result;
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            default:
+                return "ERROR";
+        }
+        if (expression.length < 4) {
+            return String.valueOf(result);
+        }
+        int c = Integer.parseInt(expression[4]);
+        switch (expression[3]) {
+            case "+":
+                result = result + c;
+                break;
+            case "-":
+                result = result - c;
+                break;
+            default:
+                return "ERROR";
+        }
+        return String.valueOf(result);
     }
 
 }
