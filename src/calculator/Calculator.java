d542ca75a55f30bd493efebdcf3b1b3b75aa6af9
Yuri <yurishchavlev@gmail.com>
2018-08-28 18:46:42 +0300

    The first commit for new calculator.

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,37 +8,24 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        int a = Integer.parseInt(expression[0]);
-        int b = Integer.parseInt(expression[2]);
-        int result;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            default:
-                return "ERROR";
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+
+            String no = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (no) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "Error";
+            }
         }
-        if (expression.length < 4) {
-            return String.valueOf(result);
-        }
-        int c = Integer.parseInt(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            default:
-                return "ERROR";
-        }
-        return String.valueOf(result);
+        return toString().valueOf(result);
+
     }
 
 }
